12b79e90b1ffef3f864434c4b546c43b2fd0931d
martou12 <martin.verschelde@edu.devinci.fr>
2025-11-28 14:46:45 +0100

    add AmericanOption

--- include/option-pricer/options/AmericanCallOption.hpp
+++ include/option-pricer/options/AmericanCallOption.hpp
@@ -1,20 +1,14 @@
 #ifndef AMERICANCALLOPTION_HPP
 #define AMERICANCALLOPTION_HPP
 
-#include <algorithm>
 #include "AmericanOption.hpp"
 
 class AmericanCallOption : public AmericanOption {
 public:
-    AmericanCallOption(double expiry, double strike) : AmericanOption(expiry, strike) {}
+    AmericanCallOption(double expiry, double strike);
 
-    OptionType getOptionType() const override {
-        return OptionType::Call;
-    }
-
-    double payoff(double spotPrice) const override {
-        return std::max(spotPrice - getStrike(), 0.0);
-    }
+    OptionType getOptionType() const override;
+    double payoff(double spotPrice) const override;
 };
 
 #endif
--- include/option-pricer/options/AmericanPutOption.hpp
+++ include/option-pricer/options/AmericanPutOption.hpp
@@ -1,20 +1,14 @@
 #ifndef AMERICANPUTOPTION_HPP
 #define AMERICANPUTOPTION_HPP
 
-#include <algorithm>
 #include "AmericanOption.hpp"
 
 class AmericanPutOption : public AmericanOption {
 public:
-    AmericanPutOption(double expiry, double strike) : AmericanOption(expiry, strike) {}
+    AmericanPutOption(double expiry, double strike);
 
-    OptionType getOptionType() const override {
-        return OptionType::Put;
-    }
-
-    double payoff(double spotPrice) const override {
-        return std::max(getStrike() - spotPrice, 0.0);
-    }
+    OptionType getOptionType() const override;
+    double payoff(double spotPrice) const override;
 };
 
 #endif
